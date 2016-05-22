37fce193f10bd736b0093be075da130afe9fc218
Christopher Giroir <cgiroir@blizzard.com>
2016-05-21 21:39:36 -0700

    Credit for custom error class

--- src/errors/notFound.js
+++ src/errors/notFound.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Custom error class based on:
+// https://gist.github.com/justmoon/15511f92e5216fa2624b
 module.exports = function NotFoundError(key) {
   Error.captureStackTrace(this, this.constructor);
   this.name = this.constructor.name;
