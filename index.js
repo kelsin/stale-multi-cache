2869368da318309e2e74b59960550b9ade466ccf
Christopher Giroir <cgiroir@blizzard.com>
2016-05-21 23:14:34 -0700

    Public api added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Entry point exposing all classes and main cache interface
+module.exports = require('./src/cache');
+module.exports.LRUMemoryStore = require('./src/stores/lru');
+module.exports.RedisStore = require('./src/stores/redis');
+module.exports.NoopStore = require('./src/stores/noop');
+module.exports.SimpleMemoryStore = require('./src/stores/simple');
